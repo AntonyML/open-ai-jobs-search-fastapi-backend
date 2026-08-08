e2cd1f0402824a252ff88314f28451b3cec31ec7
AntonyML <antonyml2016@gmail.com>
2026-08-08 12:28:54 -0600

    fix: update asyncpg version constraint and improve run_api script structure

--- scripts/run_api.py
+++ scripts/run_api.py
@@ -1,7 +1,9 @@
 """Start the API principal server."""
-import sys, asyncio
-sys.path.insert(0, '.')
-asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-from app.main import create_app
+import sys
+
 import uvicorn
-uvicorn.run(create_app(), port=8000, host='127.0.0.1', log_level='info')
+
+sys.path.insert(0, ".")
+from app.main import create_app
+
+uvicorn.run(create_app(), port=8000, host="127.0.0.1", log_level="info")
